863924ca880b1f853a303011505b70be358a0ac9
Jibon Ahmed <jayed.swe@gmail.com>
2021-02-02 20:33:11 +0600

    register blog admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import BlogBanner
 
-# Register your models here.
+
+class BlogBannerResource(resources.ModelResource):
+    class Meta:
+        model = BlogBanner
+        fields = ('id', 'largeDevices', 'mediumDevices', 'smallDevices', 'value', 'url_field')
+
+
+class BlogBannerResourceAdmin(ImportExportModelAdmin):
+    resource_class = BlogBannerResource
+    list_display = ['title', 'url_field', 'value', 'timestamp']
+    list_editable = ['value']
+
+
+admin.site.register(BlogBanner, BlogBannerResourceAdmin)
